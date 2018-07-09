3a6584fdf2975140c7c9d4fb4b245af28b6bea89
Nell <dashuai.fu@loncus.com>
2018-07-09 23:20:03 +0800

    修改index

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import RecyclerView from './src/listview-android/RecyclerViewList';
 import DataSource from './src/listview-android/DataSource';
-var Views = {RecyclerView, DataSource}
+import TableView from './src/listview-ios/TableView'
+import TableViewSection from './src/listview-ios/TableViewSection'
+import TableViewHeader from './src/listview-ios/TableViewHeader'
+import TableViewFooter from './src/listview-ios/TableViewFooter'
+import TableViewCell from './src/listview-ios/TableViewCell'
+import TableViewItem from './src/listview-ios/TableViewItem'
+import RNTableViewConsts from './src/listview-ios/TableViewConsts'
+
+var ListViewIos = {
+  TableView,
+  Item: TableViewItem,
+  Footer: TableViewFooter,
+  Header: TableViewHeader,
+  Cell: TableViewCell,
+  Section: TableViewSection,
+  Consts:  RNTableViewConsts
+}
+var Views = {
+  RecyclerView,
+  DataSource,
+  ListViewIos
+}
 
 module.exports = Views;
